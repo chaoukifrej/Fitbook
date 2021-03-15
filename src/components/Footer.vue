ab7f667d7a4225d6f1b8301411136ff021039c06
alexandre Dalmar <alexandredalmar@gmail.com>
2021-03-15 22:25:14 +0100

    ajout icone home dans le footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="footer">
-    <router-link to="/">Actus</router-link> |
-    <router-link to="/Ajoutpost">Ajoutpost</router-link> |
+    <router-link to="/"
+      ><font-awesome-icon :icon="['fas', 'home']"
+    /></router-link>
+    | <router-link to="/Ajoutpost">Ajoutpost</router-link> |
     <router-link to="/Perso">Perso</router-link>
   </div>
 </template>
