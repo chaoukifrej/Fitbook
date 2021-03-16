2d60b8058b41e5cb28e6a00364e9ecbe7a890e31
alexandre Dalmar <alexandredalmar@gmail.com>
2021-03-16 11:52:42 +0100

    j'ai mis une image utilisateur dans l'assts, commmencé le visu de la page ajoutPoste, il me reste a checker la taille de l'image, et finir le scss

--- src/views/Ajoutpost.vue
+++ src/views/Ajoutpost.vue
@@ -1,9 +1,58 @@
 <template>
-  <div class="ajoutpost"></div>
+  <div class="ajoutpost">
+    <Header />
+    <div class="ajoutphoto">
+      <label for="files" class="btn"
+        ><img
+          src="../assets/imgUtilisateur.png"
+          alt="imgUtilisateur"
+          class="imgUtilisateur"
+      /></label>
+      <input id="files" style="visibility:hidden;" type="file" />
+      <div class="description">
+        <textarea name="description" id="" cols="30" rows="10">
+Entré votre description</textarea
+        >
+      </div>
+
+      <input type="text" placeholder="lieu" />
+
+      <button class="btnValider">VALIDER</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.ajoutpost {
+  input[type="file"] {
+    color: transparent;
+  }
+
+  .ajoutphoto {
+    flex-direction: column;
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+
+    .imgUtilisateur {
+      width: 100%;
+    }
+
+    .description {
+      width: 100%;
+    }
+    textarea {
+      width: 100%;
+    }
+  }
+}
+</style>
