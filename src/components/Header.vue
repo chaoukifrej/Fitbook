a5532acdfd3029a349d545cf194c10e4716f9a57
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-03-16 11:10:59 +0100

    boutton inscription dans Header.vue, ajout import icone dans main.js, ajout header dans connexion

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
-    <img id="img" alt="logo FitBook" src="../assets/fitbookCoupe.png" />
+    <img id="imgLogo" alt="logo FitBook" src="../assets/fitbookCoupe.png" />
+    <button class="inscription">Incription</button>
   </div>
 </template>
 
@@ -8,11 +9,28 @@
 export default {};
 </script>
 
-<style>
-#img {
-  margin-top: 8px;
-  margin-left: 15px;
-  height: 35px;
+<style lang="scss">
+.header {
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 15px;
+  .inscription {
+    font-size: 1.1rem;
+    padding: 3px 15px;
+    border: 2px solid #ff1616;
+    background-color: transparent;
+    border-radius: 5px;
+    color: #ff1616;
+    outline: none;
+    transition: 0.4s;
+    &:active {
+      transform: scale(0.98);
+      color: whitesmoke;
+    }
+  }
+  #imgLogo {
+    height: 35px;
+  }
 }
 </style>
--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -5,7 +5,12 @@
 </template>
 
 <script>
-export default {};
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
 </script>
 
 <style lang="scss"></style>
