6cbde92bf3e77eeb116e2178eb7782b6945a43cc
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-03-16 16:27:50 +0100

    modifprofil.vue

--- src/views/Perso.vue
+++ src/views/Perso.vue
@@ -1,31 +1,7 @@
 <template>
   <div class="perso">
     <Header />
-    <div class="amodifier">
-      <div class="ville">
-        <label for="ville">Ville <span class="opt">(optionnel)</span> </label>
-        <input type="text" placeholder="Nice" />
-      </div>
-      <div class="sport">
-        <label for="sport"
-          >Sport pratiqué <span class="opt">(optionnel)</span></label
-        >
-        <input type="text" placeholder="Body-Building" />
-      </div>
-      <div class="status">
-        <label for="status">Status <span class="opt">(optionnel)</span></label>
-        <select name="status" id="status">
-          <option value="amateur">Amateur</option>
-          <option value="pro">Professionnelle</option>
-        </select>
-      </div>
-      <div class="salleSport">
-        <label for="salleSport"
-          >Salle de sport <span class="opt">(optionnel)</span>
-        </label>
-        <input type="text" placeholder="Ta salle préferée" />
-      </div>
-    </div>
+
     <Footer />
   </div>
 </template>
