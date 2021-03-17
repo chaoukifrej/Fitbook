2009855fcede076af78117747173fb70a603e645
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-03-17 10:19:38 +0100

    Debut creation tableau users et methods

--- src/main.js
+++ src/main.js
@@ -27,5 +27,47 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  data: {
+    users: [
+      {
+        firstname: "Jean Onche la tapette",
+        lastname: "15/03/20",
+        email: "jot@email.fr",
+        age: "26",
+        img:
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2F-TkKO4S3bFZU%2FAAAAAAAAAAI%2FAAAAAAAAAAA%2FdfhYjIlWsO4%2Fs900-c-k-no-mo-rj-c0xffffff%2Fphoto.jpg&f=1&nofb=1",
+        description:
+          "Fin de séance, 21k de poussé !!! De la pure folie cette seance de merde",
+        nbLike: "12",
+        nbComment: "3",
+        isConnected: false,
+      },
+    ],
+  },
+  methods: {
+    pushUsers(p) {
+      let user = {
+        firstname: p.firstname,
+        lastname: p.lastname,
+        email: p.email,
+        password: p.password,
+      };
+      let check = false;
+      for (const elem of this.users) {
+        if (elem.email != user.email) {
+          check = false;
+        } else {
+          check = true;
+          break;
+        }
+      }
+      if (!check) {
+        if (user.lastname != "") {
+          this.users.push(user);
+        }
+      }
+      console.log(user);
+    },
+  },
   render: (h) => h(App),
 }).$mount("#app");
