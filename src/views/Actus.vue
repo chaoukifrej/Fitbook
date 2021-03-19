1627715f6ceeba8fbbe5630bc84b11a746ec3330
alexandre Dalmar <alexandredalmar@gmail.com>
2021-03-19 16:11:17 +0100

    affichage page actu

--- src/views/Actus.vue
+++ src/views/Actus.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="actus">
     <Header />
-    <!--     <Post />
-    <Post />
-    <Post />
-    <Post />
-    <Post /> -->
 
+    <div v-for="post in posts" :key="post._id">
+      <Post :post="post" />
+      <Post />
+    </div>
     <Footer />
   </div>
 </template>
@@ -15,14 +14,37 @@
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
-//import Post from "@/components/Post.vue";
+import Post from "@/components/Post.vue";
 
 export default {
   name: "Actus",
   components: {
     Header,
     Footer,
-    //Post,
+    Post,
+  },
+
+  data: () => ({ posts: [] }),
+
+  mounted: async function() {
+    const options = {
+      method: "GET",
+    };
+    try {
+      const response = await fetch(
+        "https://fitbook-api.osc-fr1.scalingo.io/posts",
+        options
+      );
+      console.log(response.status);
+
+      const data = await response.json();
+      console.log(data);
+
+      this.posts = data.posts;
+      console.log(this.posts);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 </script>
